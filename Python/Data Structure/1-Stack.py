ebe4ba673fb6fd5dd8b2d47fbda9c5a5fbdd9232
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2022-08-27 23:59:06 +0900

    Implementation using list,LifoQueue, Dequeue and Single Linked List

--- Python/Data Structure/1-Stack.py
+++ Python/Data Structure/1-Stack.py
@@ -13,7 +13,7 @@ print(stack) # ['a', 'b', 'c']
 print(stack.pop()) # 'c'
 print(stack.pop()) # 'b'
 print(stack.pop()) # 'a'
-
+print("="*40)
 
 # Implementation by collections.deque
 
@@ -32,6 +32,7 @@ print(stack) # ['a', 'b', 'c']
 print(stack.pop()) # 'c'
 print(stack.pop()) # 'b'
 print(stack.pop()) # 'a'
+print("="*40)
 
 # Implementation by queue.LifoQueue
 
@@ -39,18 +40,81 @@ from queue import LifoQueue
 
 stack = LifoQueue(3)
 
-print(stack.qsize())
+print(f"LifoQueue Size: {stack.qsize()}")
 
 stack.put('a')
 stack.put('b')
 stack.put('c')
 
-print("Full: ", stack.full())
-print("Size: ", stack.qsize())
+print(f"Full: {stack.full()}")
+print(f"Size: {stack.qsize()}")
 
 # LIFO
 print(stack.get()) # 'c'
 print(stack.get()) # 'b'
 print(stack.get()) # 'a'
 
-print("Empty: ", stack.empty())
+print(f"Empty: {stack.empty()}")
+print("="*40)
+
+# Implementation using a singly linked list
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class Stack:
+    def __init__(self):
+        self.head = Node("head")
+        self.size = 0
+    
+    # String representation of the stack
+    def __str__(self):
+        cur = self.head.next
+        out = ""
+        while cur:
+            out += str(cur.value) + "->"
+            cur = cur.next
+        return out[:-3]
+
+    # Get the current size of stack
+    def getSize(self):
+        return self.size
+    
+    # Check if the stack is empty
+    def isEmpty(self):
+        return self.size == 0
+    
+    # Get the top item of the stack
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Stack is Empty")
+        return self.head.next.value
+    
+    # Push a value into the stack
+    def push(self, value):
+        node = Node(value)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+    
+    # Remove a value from the stack and return
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Stack is Empty")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+if __name__ == '__main__':
+    stack = Stack()
+    for i in range(1, 11):
+        stack.push(i)
+    print(f'Stack: {stack}')
+
+    for _ in range(1, 6):
+        remove = stack.pop()
+        print(f'Pop: {remove}')
+    print(f'Rest of Stack: {stack}')
