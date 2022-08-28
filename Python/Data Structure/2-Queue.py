01f58448ec4c042593917f7db14429ad9e2c4bea
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2022-08-28 09:52:15 +0900

    Implementation using list, collections.deque, queue.Queue

---
+++ Python/Data Structure/2-Queue.py
@@ -0,0 +1,55 @@
+# Implementaion using list
+
+queue = []
+
+queue.append('a')
+queue.append('b')
+queue.append('c')
+
+print(queue)
+
+# pop() operations of list provides O(n) time complexity 
+print(queue.pop(0)) # 'a'
+print(queue.pop(0)) # 'b'
+print(queue.pop(0)) # 'c'
+print("="*40)
+
+# Implementation using collections.deque
+
+from collections import deque
+
+q = deque()
+
+q.append('a')
+q.append('b')
+q.append('c')
+
+print(q)
+
+# pop() operations of deque provides O(1) time complexity 
+print(q.popleft()) # 'a'
+print(q.popleft()) # 'b'
+print(q.popleft()) # 'c'
+print("="*40)
+
+# Implementation using queue.Queue
+
+from queue import Queue
+
+q = Queue(maxsize=3)
+
+print(q.qsize())
+
+q.put('a')
+q.put('b')
+q.put('c')
+
+print(f"Full: {q.full()}")
+
+print(q.get()) # 'a'
+print(q.get()) # 'b'
+print(q.get()) # 'c'
+
+q.put(1)
+print(f"Empty: {q.empty()}")
+print(f"Full: {q.full()}")
